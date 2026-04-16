d822783683a14fa6cf3eeca7cffbae618c20e11a
zayd-es <elabbouszayd@gmail.com>
2026-04-16 10:42:25 +0100

    feat(ui): enhance work section responsiveness and theme adaptivity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       viewport={{ once: true }}
       transition={{ duration: 1 }}
       id="about"
-      className="w-full px-6 md:px-[9%] py-10 scroll-mt-20 overflow-hidden"
+      className="w-full px-6 md:px-[9%]  scroll-mt-20 overflow-hidden"
     >
       <motion.h4
         initial={{ opacity: 0, y: -20 }}
@@ -32,30 +32,30 @@ const About = () => {
       >
         about me
       </motion.h2>
-
       <div className="flex flex-col lg:flex-row items-center lg:items-start gap-12 lg:gap-10 my-16 md:my-20">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           whileInView={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.6, delay: 0.7 }}
-          className="w-full max-w-[320px] md:max-w-[380px] shrink-0"
+          className="w-fit mx-auto lg:mx-0 shrink-0"
         >
-          <ProfileCard
-            name="Zayd Elabbous"
-            title="Front-end Developer"
-            status="Online"
-            avatarUrl="/meZayd.jpeg"
-            avatarClassName="object-bottom"
-            contactText="Contact Me"
-            showUserInfo={false}
-            enableTilt={true}
-            enableMobileTilt={false}
-            onContactClick={() => console.log("Contact clicked")}
-            behindGlowColor="rgba(125, 190, 255, 0.67)"
-            iconUrl="/assets/demo/iconpattern.png"
-            behindGlowEnabled
-            innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
-          />
+          <div className="w-[260px] sm:w-[300px] md:w-[380px] mx-auto lg:mx-0">
+            <ProfileCard
+              name="Zayd Elabbous"
+              title="Front-end Developer"
+              status="Online"
+              avatarUrl="/meZayd.jpeg"
+              avatarClassName="object-bottom"
+              contactText="Contact Me"
+              showUserInfo={false}
+              enableTilt={true}
+              enableMobileTilt={false}
+              onContactClick={() => console.log("Contact clicked")}
+              behindGlowColor="rgba(125, 190, 255, 0.67)"
+              behindGlowEnabled
+              innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
+            />
+          </div>
         </motion.div>
 
         <motion.div
